36e515bdfaf92d306c0fe23e5aa1781a36279469
K-Zoldyck <devv-robert@outlook.com>
2024-10-25 21:50:39 -0300

    ..

--- projc/public/index.php
+++ projc/public/index.php
@@ -3,4 +3,9 @@
     $db_pass = $_ENV['DB_PASS'];
     $db_name = $_ENV['DB_NAME'];
     $db_host = $_ENV['DB_HOST'];
+
+    echo $db_user;
+    echo $db_pass;
+    echo $db_name;
+    echo $db_host;
 ?>
